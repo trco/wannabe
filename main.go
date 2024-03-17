be6d4c1ca6f23741a82312a233649ef99da75fdc
trco <uros.trstenjak@gmail.com>
2024-03-17 10:42:55 +0100

    update fatal error handling in main

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ import (
 
 func main() {
 	// TODO read config path from env variable
-	config := config.Load("config.json")
+	config, err := config.Load("config.json")
+	if err != nil {
+		log.Fatalf("fatal error starting app: %v", err)
+	}
 
 	storageProvider, err := providers.StorageProviderFactory(config.StorageProvider)
 	if err != nil {
-		log.Fatalf("fatal error when starting app: %v", err)
+		log.Fatalf("fatal error starting app: %v", err)
 	}
 
 	app := fiber.New()
@@ -25,21 +28,13 @@ func main() {
 	app.Get("/wannabe/readiness", handlers.Readiness)
 
 	// Api endpoints
-	// NOTE
 	app.Get("/wannabe/api/records/:hash", handlers.GetRecords(storageProvider))
 	app.Get("/wannabe/api/records", handlers.GetRecords(storageProvider))
 	app.Post("/wannabe/api/records", handlers.PostRecords(config, storageProvider))
 	app.Delete("/wannabe/api/records/:hash", handlers.DeleteRecords(storageProvider))
 	app.Delete("/wannabe/api/records", handlers.DeleteRecords(storageProvider))
 
-	// NOTE get all hashes/curls
-	// app.Get("/wannabe/api/hash/curl", handlers.Api)
-
-	// NOTE run all requests and validate responses
-	// app.Get("/wannabe/api/records/test", handlers.Api)
-
 	// Tools endpoints
-	// NOTE regenerate records using new config
 	app.Get("/wannabe/tools/regenerate", handlers.Regenerate(app, config, storageProvider))
 
 	// Wannabe endpoints
