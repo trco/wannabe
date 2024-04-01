48930bf4477f9a79bb175bb45661b155fcb7ea74
trco <uros.trstenjak@gmail.com>
2024-04-01 15:01:07 +0200

    add and cleanup tests in record package

---
+++ record/actions/validate_records_test.go
@@ -0,0 +1,124 @@
+package actions
+
+import (
+	"testing"
+	"time"
+	"wannabe/config"
+	"wannabe/record/entities"
+)
+
+func TestValidateRecords(t *testing.T) {
+	// valid record
+	validationErrors, _ := ValidateRecords(testConfigB, []entities.Record{validRecord})
+
+	if validationErrors[0] != "" {
+		t.Errorf("validation failed although it should not, validationErrors: %v", validationErrors)
+	}
+
+	// invalid record
+	validationErrors, _ = ValidateRecords(testConfigB, []entities.Record{invalidRecord})
+	expectedErrs := "Key: 'Record.Request.Host' Error:Field validation for 'Host' failed on the 'host_not_matching_config_server' tag"
+
+	if validationErrors[0] != expectedErrs {
+		t.Errorf("validation succeeded although it should not, validationErrors: %v", validationErrors)
+	}
+}
+
+// reusable variables
+
+var testConfigB = config.Config{
+	StorageProvider: config.StorageProvider{
+		Type:       "filesystem",
+		Regenerate: false,
+		FilesystemConfig: config.FilesystemConfig{
+			Folder:           "records",
+			RegenerateFolder: "",
+			Format:           "json",
+		},
+	},
+	Read: config.Read{
+		Enabled:     true,
+		FailOnError: true,
+	},
+	Server: "https://analyticsdata.googleapis.com",
+}
+
+var validRecord = entities.Record{
+	Request: entities.Request{
+		HttpMethod: "POST",
+		Host:       "https://analyticsdata.googleapis.com",
+		Path:       "test",
+		Query: map[string]string{
+			"test": "test",
+		},
+		Headers: map[string][]string{
+			"Content-Type": {"application/json"},
+		},
+		Body: map[string]interface{}{
+			"test": "test",
+		},
+	},
+	Response: entities.Response{
+		StatusCode: 200,
+		Headers: map[string][]string{
+			"Content-Type": {"application/json"},
+		},
+		Body: map[string]interface{}{
+			"test": "test",
+		},
+	},
+	Metadata: entities.Metadata{
+		RequestedAt: entities.Timestamp{
+			Unix: 0,
+			UTC:  time.Time{},
+		},
+		GeneratedAt: entities.Timestamp{
+			Unix: 0,
+			UTC:  time.Time{},
+		},
+		RegeneratedAt: entities.Timestamp{
+			Unix: 0,
+			UTC:  time.Time{},
+		},
+	},
+}
+
+var invalidRecord = entities.Record{
+	Request: entities.Request{
+		HttpMethod: "POST",
+		Host:       "https://test.com",
+		Path:       "test",
+		Query: map[string]string{
+			"test": "test",
+		},
+		Headers: map[string][]string{
+			"Content-Type": {"application/json"},
+		},
+		Body: map[string]interface{}{
+			"test": "test",
+		},
+	},
+	Response: entities.Response{
+		StatusCode: 200,
+		Headers: map[string][]string{
+			"Content-Type": {"application/json"},
+		},
+		Body: map[string]interface{}{
+			"test": "test",
+		},
+	},
+	Metadata: entities.Metadata{
+		RequestedAt: entities.Timestamp{
+			Unix: 0,
+			UTC:  time.Time{},
+		},
+		GeneratedAt: entities.Timestamp{
+			Unix: 0,
+			UTC:  time.Time{},
+		},
+		RegeneratedAt: entities.Timestamp{
+			Unix: 0,
+			UTC:  time.Time{},
+		},
+	},
+}
--- record/services/generate_record_test.go
+++ record/services/generate_record_test.go
@@ -8,7 +8,24 @@ import (
 	"wannabe/record/entities"
 )
 
-var testConfig = config.Records{
+func TestGenerateRecord(t *testing.T) {
+	encodedRecord, _ := GenerateRecord(testConfigA, payload)
+
+	var record entities.Record
+
+	_ = json.Unmarshal(encodedRecord, &record)
+
+	if !reflect.DeepEqual(expectedRecordC.Request, record.Request) {
+		t.Errorf("Expected record request: %v, Actual record request: %v", expectedRecordC, record)
+	}
+
+	if !reflect.DeepEqual(expectedRecordC.Response, record.Response) {
+		t.Errorf("Expected record response: %v, Actual record response: %v", expectedRecordC, record)
+	}
+}
+
+// reusable variables
+var testConfigA = config.Records{
 	Headers: config.HeadersToExclude{
 		Exclude: []string{},
 	},
@@ -27,57 +44,43 @@ var payload = entities.GenerateRecordPayload{
 		"Content-Type": {"application/json"},
 		"Accept":       {"test"},
 	},
+	// {"test":"test"}
 	RequestBody: []byte{123, 10, 32, 32, 32, 32, 34, 116, 101, 115, 116, 34, 58, 32, 34, 116, 101, 115, 116, 34, 10, 125},
 	StatusCode:  200,
 	ResponseHeaders: map[string][]string{
 		"Content-Type": {"application/json"},
 		"Accept":       {"test"},
 	},
+	// {"test":"test"}
 	ResponseBody: []byte{123, 10, 32, 32, 32, 32, 34, 116, 101, 115, 116, 34, 58, 32, 34, 116, 101, 115, 116, 34, 10, 125},
 }
 
-func TestGenerateRecord(t *testing.T) {
-	expected := entities.Record{
-		Request: entities.Request{
-			Hash:       "testHash",
-			Curl:       "testCurl",
-			HttpMethod: "POST",
-			Host:       "test.com",
-			Path:       "/test",
-			Query: map[string]string{
-				"test": "test",
-			},
-			Headers: map[string][]string{
-				"Content-Type": {"application/json"},
-				"Accept":       {"test"},
-			},
-			Body: map[string]interface{}{
-				"test": "test",
-			},
+var expectedRecordC = entities.Record{
+	Request: entities.Request{
+		Hash:       "testHash",
+		Curl:       "testCurl",
+		HttpMethod: "POST",
+		Host:       "test.com",
+		Path:       "/test",
+		Query: map[string]string{
+			"test": "test",
 		},
-		Response: entities.Response{
-			StatusCode: 200,
-			Headers: map[string][]string{
-				"Content-Type": {"application/json"},
-				"Accept":       {"test"},
-			},
-			Body: map[string]interface{}{
-				"test": "test",
-			},
+		Headers: map[string][]string{
+			"Content-Type": {"application/json"},
+			"Accept":       {"test"},
 		},
-	}
-
-	encodedRecord, _ := GenerateRecord(testConfig, payload)
-
-	var record entities.Record
-
-	_ = json.Unmarshal(encodedRecord, &record)
-
-	if !reflect.DeepEqual(expected.Request, record.Request) {
-		t.Errorf("Expected record request: %v, Actual record request: %v", expected, record)
-	}
-
-	if !reflect.DeepEqual(expected.Response, record.Response) {
-		t.Errorf("Expected record response: %v, Actual record response: %v", expected, record)
-	}
+		Body: map[string]interface{}{
+			"test": "test",
+		},
+	},
+	Response: entities.Response{
+		StatusCode: 200,
+		Headers: map[string][]string{
+			"Content-Type": {"application/json"},
+			"Accept":       {"test"},
+		},
+		Body: map[string]interface{}{
+			"test": "test",
+		},
+	},
 }
