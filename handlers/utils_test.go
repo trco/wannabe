a5580885282d0d0185a0d7eebc12d3b6fb9ccc28
trco <uros.trstenjak@gmail.com>
2024-04-01 11:58:36 +0200

    add utils tests to handlers package

---
+++ handlers/utils_test.go
@@ -0,0 +1,69 @@
+package handlers
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/valyala/fasthttp"
+)
+
+type TestError struct {
+	message string
+}
+
+func (e *TestError) Error() string {
+	return e.message
+}
+
+func TestInternalError(t *testing.T) {
+	app := fiber.New()
+	ctx := app.AcquireCtx(&fasthttp.RequestCtx{})
+
+	testError := &TestError{"test error"}
+
+	_ = internalError(ctx, testError)
+
+	expectedStatusCode := 500
+	expectedResponseBody := "{\"statusCode\":500,\"error\":\"test error\"}"
+
+	if !reflect.DeepEqual(expectedStatusCode, ctx.Response().StatusCode()) {
+		t.Errorf("expected status code: %v, actual status code: %v", expectedStatusCode, ctx.Response().StatusCode())
+	}
+
+	if !reflect.DeepEqual(expectedResponseBody, string(ctx.Response().Body())) {
+		t.Errorf("expected response body: %v, actual response body: %v", expectedResponseBody, string(ctx.Response().Body()))
+	}
+}
+
+func TestCheckDuplicates(t *testing.T) {
+	// duplicates exist
+	slice := []string{"a", "b", "c"}
+
+	duplicatesExist := checkDuplicates(slice, "a")
+
+	if duplicatesExist != true {
+		t.Errorf("no duplicates detected although they are present in slice")
+	}
+
+	// no duplicates
+	duplicatesExist = checkDuplicates(slice, "d")
+
+	fmt.Println(duplicatesExist)
+
+	if duplicatesExist == true {
+		t.Errorf("duplicates detected although they are not present")
+	}
+}
+
+func TestProcessRecordValidation(t *testing.T) {
+	count := 0
+	var recordProcessingDetails []RecordProcessingDetails
+
+	processRecordValidation(&recordProcessingDetails, "test hash", "test message", &count)
+
+	if recordProcessingDetails[0].Hash != "test hash" || recordProcessingDetails[0].Message != "test message" || count != 1 {
+		t.Errorf("record processing details not valid, expected: hash: 'test hash', message: 'test message', count: 1, actual: hash: '%v', message: '%v', count: %v", recordProcessingDetails[0].Hash, recordProcessingDetails[0].Message, count)
+	}
+}
