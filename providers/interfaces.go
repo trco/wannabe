44703e18e57bc2cb253803ac7a7891de2f848716
trco <uros.trstenjak@gmail.com>
2024-04-07 09:37:00 +0200

    remove obsolete function

--- providers/interfaces.go
+++ providers/interfaces.go
@@ -1,11 +1,6 @@
 package providers
 
-import (
-	"wannabe/config"
-)
-
 type StorageProvider interface {
-	GetConfig() config.StorageProvider
 	ReadRecords(hashes []string) ([][]byte, error)
 	InsertRecords(hashes []string, records [][]byte) error
 	DeleteRecords(hashes []string) error
