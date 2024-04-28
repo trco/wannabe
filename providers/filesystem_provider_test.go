18e2548ae97f93f57f3ec1def23baff830b36b42
trco <uros.trstenjak@gmail.com>
2024-04-28 22:28:24 +0200

    update filesystem provider test

--- providers/filesystem_provider_test.go
+++ providers/filesystem_provider_test.go
@@ -15,18 +15,22 @@ var testConfig = config.Config{
 			Format:           "json",
 		},
 	},
+	Wannabes: map[string]config.Wannabe{
+		"test.api.com": {},
+	},
 }
 
 var filesystemProvider = FilesystemProvider{
-	Config: testConfig.StorageProvider,
+	Config: testConfig,
 }
 
 var testRecord = []byte{53}
 
 func TestInsertAndReadRecord(t *testing.T) {
-	_ = filesystemProvider.InsertRecords([]string{"testHash3"}, [][]byte{testRecord})
+	_ = filesystemProvider.CreateFolders()
+	_ = filesystemProvider.InsertRecords([][]byte{testRecord}, []string{"testHash3"}, "test.api.com")
 
-	records, _ := filesystemProvider.ReadRecords([]string{"testHash3"})
+	records, _ := filesystemProvider.ReadRecords([]string{"testHash3"}, "test.api.com")
 
 	if len(records) == 0 {
 		t.Errorf("failed inserting and reading records")
@@ -34,25 +38,27 @@ func TestInsertAndReadRecord(t *testing.T) {
 }
 
 func TestDeleteRecord(t *testing.T) {
-	filesystemProvider.InsertRecords([]string{"testHash4"}, [][]byte{testRecord})
+	_ = filesystemProvider.CreateFolders()
+	filesystemProvider.InsertRecords([][]byte{testRecord}, []string{"testHash4"}, "test.api.com")
 
-	filesystemProvider.DeleteRecords([]string{"testHash4"})
+	filesystemProvider.DeleteRecords([]string{"testHash4"}, "test.api.com")
 
-	fileContents, _ := filesystemProvider.ReadRecords([]string{"testHash4"})
+	fileContents, _ := filesystemProvider.ReadRecords([]string{"testHash4"}, "test.api.com")
 
 	if len(fileContents) != 0 {
 		t.Errorf("failed deleting record")
 	}
 
-	filesystemProvider.DeleteRecords([]string{"testHash4"})
+	filesystemProvider.DeleteRecords([]string{"testHash4"}, "test.api.com")
 }
 
 func TestGetHashes(t *testing.T) {
-	_ = filesystemProvider.InsertRecords([]string{"testHash5"}, [][]byte{testRecord})
-	_ = filesystemProvider.InsertRecords([]string{"testHash6"}, [][]byte{testRecord})
-	_ = filesystemProvider.InsertRecords([]string{"testHash7"}, [][]byte{testRecord})
+	_ = filesystemProvider.CreateFolders()
+	_ = filesystemProvider.InsertRecords([][]byte{testRecord}, []string{"testHash5"}, "test.api.com")
+	_ = filesystemProvider.InsertRecords([][]byte{testRecord}, []string{"testHash6"}, "test.api.com")
+	_ = filesystemProvider.InsertRecords([][]byte{testRecord}, []string{"testHash7"}, "test.api.com")
 
-	hashes, _ := filesystemProvider.GetHashes()
+	hashes, _ := filesystemProvider.GetHashes("test.api.com")
 
 	expectedHashes := []string{"testHash5", "testHash6", "testHash7"}
 
@@ -64,9 +70,9 @@ func TestGetHashes(t *testing.T) {
 }
 
 func TestGenerateFilepath(t *testing.T) {
-	generateFilepath := filesystemProvider.generateFilepath("testHash1")
+	generateFilepath := filesystemProvider.generateFilepath("testHash1", "test.api.com")
 
-	expectedFilepath := "/var/folders/6z/9bvblj5j2s9bngjcnr18jls80000gn/T/testHash1.json"
+	expectedFilepath := "/var/folders/6z/9bvblj5j2s9bngjcnr18jls80000gn/T/test.api.com/testHash1.json"
 
 	if expectedFilepath != generateFilepath {
 		t.Errorf("expected generate filepath: %v, actual generate filepath: %v", expectedFilepath, generateFilepath)
@@ -74,17 +80,17 @@ func TestGenerateFilepath(t *testing.T) {
 }
 
 func TestGenerateFilepathRegenerate(t *testing.T) {
-	filesystemProvider.Config.Regenerate = true
+	filesystemProvider.Config.StorageProvider.Regenerate = true
 
-	regenerateFilepath := filesystemProvider.generateFilepathRegenerate("testHash2")
+	regenerateFilepath := filesystemProvider.generateFilepathRegenerate("testHash2", "test.api.com")
 
-	expectedFilepath := "/var/folders/6z/9bvblj5j2s9bngjcnr18jls80000gn/T/regenerate/testHash2.json"
+	expectedFilepath := "/var/folders/6z/9bvblj5j2s9bngjcnr18jls80000gn/T/regenerate/test.api.com/testHash2.json"
 
 	if expectedFilepath != regenerateFilepath {
 		t.Errorf("expected regenerate filepath: %v, actual regenerate filepath: %v", expectedFilepath, regenerateFilepath)
 	}
 
-	filesystemProvider.Config.Regenerate = false
+	filesystemProvider.Config.StorageProvider.Regenerate = false
 }
 
 // reusable functions
