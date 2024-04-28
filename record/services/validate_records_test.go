b07545e1ac2dcc2c0310a8a43816a711d2476f50
trco <uros.trstenjak@gmail.com>
2024-04-28 22:27:13 +0200

    update record tests

--- record/services/validate_records_test.go
+++ record/services/validate_records_test.go
@@ -9,15 +9,15 @@ import (
 
 func TestValidateRecords(t *testing.T) {
 	// valid record
-	validationErrors, _ := ValidateRecords(testConfigB, []entities.Record{validRecord})
+	validationErrors, _ := ValidateRecords(wannabeB, []entities.Record{validRecord})
 
 	if validationErrors[0] != "" {
 		t.Errorf("validation failed although it should not, validationErrors: %v", validationErrors)
 	}
 
 	// invalid record
-	validationErrors, _ = ValidateRecords(testConfigB, []entities.Record{invalidRecord})
-	expectedErrs := "Key: 'Record.Request.Host' Error:Field validation for 'Host' failed on the 'host_not_matching_config_server' tag"
+	validationErrors, _ = ValidateRecords(wannabeB, []entities.Record{invalidRecord})
+	expectedErrs := "Key: 'Record.Request.Host' Error:Field validation for 'Host' failed on the 'host_not_matching_wannabe_server' tag"
 
 	if validationErrors[0] != expectedErrs {
 		t.Errorf("validation succeeded although it should not, validationErrors: %v", validationErrors)
@@ -26,20 +26,7 @@ func TestValidateRecords(t *testing.T) {
 
 // reusable variables
 
-var testConfigB = config.Config{
-	Mode:            "server",
-	FailOnReadError: true,
-	StorageProvider: config.StorageProvider{
-		Type:       "filesystem",
-		Regenerate: false,
-		FilesystemConfig: config.FilesystemConfig{
-			Folder:           "records",
-			RegenerateFolder: "",
-			Format:           "json",
-		},
-	},
-	Server: "https://analyticsdata.googleapis.com",
-}
+var wannabeB = config.Wannabe{}
 
 var validRecord = entities.Record{
 	Request: entities.Request{
