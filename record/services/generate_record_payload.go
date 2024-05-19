36b4d9e1c607a7a82ef7f48bd67ab2601cfb34fd
trco <uros.trstenjak@gmail.com>
2024-05-19 19:40:44 +0200

    add missing action and test

--- record/services/generate_record_payload.go
+++ record/services/generate_record_payload.go
@@ -1,46 +1,10 @@
 package services
 
 import (
-	"bytes"
-	"io"
+	"wannabe/record/actions"
 	"wannabe/types"
 )
 
 func GenerateRecordPayload(wannabeSession types.WannabeSession, hash string, curl string) (types.RecordPayload, error) {
-	request := wannabeSession.Request()
-	response := wannabeSession.Response()
-
-	requestBody, err := io.ReadAll(request.Body)
-	if err != nil {
-		return types.RecordPayload{}, err
-	}
-	defer request.Body.Close()
-
-	// set body back to request
-	request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
-
-	responseBody, err := io.ReadAll(response.Body)
-	if err != nil {
-		return types.RecordPayload{}, err
-	}
-	defer response.Body.Close()
-
-	// set body back to response
-	response.Body = io.NopCloser(bytes.NewBuffer(responseBody))
-
-	recordPayload := types.RecordPayload{
-		Hash:            hash,
-		Curl:            curl,
-		HttpMethod:      request.Method,
-		Host:            request.URL.Host,
-		Path:            request.URL.Path,
-		Query:           request.URL.Query(),
-		RequestHeaders:  request.Header,
-		RequestBody:     requestBody,
-		StatusCode:      response.StatusCode,
-		ResponseHeaders: response.Header,
-		ResponseBody:    responseBody,
-	}
-
-	return recordPayload, nil
+	return actions.GenerateRecordPayload(wannabeSession, hash, curl)
 }
