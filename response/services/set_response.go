b9383881fcd20bdd3d5a5e422d789532d7cfe3c8
trco <uros.trstenjak@gmail.com>
2024-05-19 19:42:59 +0200

    cleanup redundant error handling in service

--- response/services/set_response.go
+++ response/services/set_response.go
@@ -6,10 +6,5 @@ import (
 )
 
 func SetResponse(encodedRecord []byte, request *http.Request) (*http.Response, error) {
-	response, err := actions.SetResponse(encodedRecord, request)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
+	return actions.SetResponse(encodedRecord, request)
 }
