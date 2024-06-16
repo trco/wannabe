937894f8d38675ba624297cc6b975044787094ef
trco <uros.trstenjak@gmail.com>
2024-06-16 17:18:33 +0200

    cleanup

--- hash/actions/generate_hash_test.go
+++ hash/actions/generate_hash_test.go
@@ -5,11 +5,11 @@ import (
 )
 
 func TestGenerateHash(t *testing.T) {
-	curl := "curl -X 'POST' -d '{\"dateRanges\":[{\"endDate\":\"2023-12-31\",\"startDate\":\"{placeholder}\"],\"dimensions\":\"{placeholder}\",\"limit\":10000,\"metrics\":[{\"name\":\"sessions\"}],\"returnPropertyQuota\":true}' -H 'Accept: test1,test2,test3' -H 'Authorization: test access token' -H 'Content-Type: application/json' -H 'X-Test-Header: test value' 'analyticsdata.googleapis.com/v1beta/properties/%7BpropertyId%7D:runReport?app=1&status=new&user=%7Bplaceholder%7D'"
-
-	want := "598532c061134aa3e816350fa27298931b72f80117541555edc1f0ad77d67566"
 
 	t.Run("prepare curl", func(t *testing.T) {
+		curl := "curl -X 'POST' -d '{\"dateRanges\":[{\"endDate\":\"2023-12-31\",\"startDate\":\"{placeholder}\"],\"dimensions\":\"{placeholder}\",\"limit\":10000,\"metrics\":[{\"name\":\"sessions\"}],\"returnPropertyQuota\":true}' -H 'Accept: test1,test2,test3' -H 'Authorization: test access token' -H 'Content-Type: application/json' -H 'X-Test-Header: test value' 'analyticsdata.googleapis.com/v1beta/properties/%7BpropertyId%7D:runReport?app=1&status=new&user=%7Bplaceholder%7D'"
+		want := "598532c061134aa3e816350fa27298931b72f80117541555edc1f0ad77d67566"
+
 		got, _ := GenerateHash(curl)
 
 		if got != want {
