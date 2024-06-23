8f5142715683f78d84b600e0598cf1964e3e8d86
trco <uros.trstenjak@gmail.com>
2024-06-23 11:54:54 +0200

    remove endpoints

--- handlers/liveness.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func Liveness(ctx *fiber.Ctx) error {
-	return ctx.SendString("I'm alive!")
-}
--- handlers/readiness.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func Readiness(ctx *fiber.Ctx) error {
-	return ctx.SendString("I'm ready!")
-}
